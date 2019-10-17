dff1120889a95f6dad04ee68438f1720f9902d78
eigc23 <56584313+eigc23@users.noreply.github.com>
2019-10-17 18:39:31 +0800

    Add files via upload

---
+++ details.mm
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  week5
+//
+//  Created by Isabelle on 10/17/19.
+//  Copyright © 2019 Isabelle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // daily budget
+    std::string name, password, address;
+    std::cout << "Enter your name\n";
+    std::cin  >> name;
+    std::cout << "Enter your password\n";
+    std::cin  >> password;
+    std::cout << "Enter your address\n";
+    std::cin  >> address;
+    std::cout << "Hi! I am " << name << ". I live at " << address << "\n";
+    
+    
+}
