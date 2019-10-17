c30fc176f8f52174bb010cba0bbc63b5a62d8eb4
eigc23 <56584313+eigc23@users.noreply.github.com>
2019-10-17 19:07:51 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Isabelle on 10/17/19.
+//  Copyright © 2019 Isabelle. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+int main(int argc, const char * argv[]) {
+    // section
+    std::string section;
+    std::cout << "Enter you Section\n";
+    std::cin  >> section;
+    std::cout << "*** Section: " << section << " ***\n";
+    return 0;
+}
